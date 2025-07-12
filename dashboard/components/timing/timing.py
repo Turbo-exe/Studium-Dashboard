6b11d85b46a43dbf48fc3d14d01df96df26bef8b
Asenbauer Felix <asenbauerfelix@outlook.com>
2025-07-12 17:40:19 +0200

    Implemented timing component

---
+++ dashboard/components/timing/timing.py
@@ -0,0 +1,22 @@
+from django_components import Component, register
+from ... import services
+
+@register("timing")
+class Timing(Component):
+    template_file = "timing/timing.html"
+    css_file = "timing/timing.css"
+
+    def get_context_data(self, **kwargs):
+        """Adds a timestamp to the context."""
+        context = super().get_context_data(**kwargs)
+        if not context:
+            context = {}
+        timing_service = services.Timing()
+        context['finishedExamsActual'] = timing_service.count_actually_finished_courses()
+        context['finishedExamsPlan'] = timing_service.count_planned_finished_courses()
+        context['activeCourses'] = timing_service.count_active_courses()
+        context['futureCourses'] = timing_service.count_future_courses()
+        context['finishedCourses'] = timing_service.count_actually_finished_courses()
+        context['acknowledgedCourses'] = timing_service.count_acknowledged_courses()
+        context["timeModel"] = timing_service.get_time_model_friendly_name()
+        return context
