71e9783f16b5512dc00789135e103a75365b7f43
Asenbauer Felix <asenbauerfelix@outlook.com>
2025-07-12 23:10:58 +0200

    Added django management command for easy database prefilling.

---
+++ dashboard/management/commands/_courses.py
@@ -0,0 +1,247 @@
+
+
+sample_courses = [
+  {
+    "course_name": "Artificial Intelligence",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 1,
+    "status": "ENR",
+    "short_code": "DLBDSEAIS01 D"
+  },
+  {
+    "course_name": "Einführung in das wissenschaftliche Arbeiten für IT und Technik",
+    "ects": 5,
+    "exam_type": "WB",
+    "semester": 1,
+    "status": "CMP",
+    "score": 90,
+    "short_code": "DLEWIRITTOI"
+  },
+  {
+    "course_name": "Einführung in die Programmierung mit Python",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 1,
+    "status": "ENR",
+    "short_code": "DLBDSIPWP01 D"
+  },
+  {
+    "course_name": "Mathematics: Analysis",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 1,
+    "status": "CMP",
+    "score": 70,
+    "short_code": "DLB81M001"
+  },
+  {
+    "course_name": "Kollaboratives Arbeiten",
+    "ects": 5,
+    "exam_type": "PR",
+    "semester": 1,
+    "status": "ACK",
+    "short_code": "DLBKA01"
+  },
+  {
+    "course_name": "Statistics-Probability and Descriptive Statistics",
+    "ects": 5,
+    "exam_type": "WB",
+    "semester": 2,
+    "status": "ENR",
+    "short_code": "DLBDSSPDS01 D"
+  },
+  {
+    "course_name": "Objektorientierte und funktionale Programmierung mit Python",
+    "ects": 5,
+    "exam_type": "PO",
+    "semester": 2,
+    "status": "ENR",
+    "short_code": "DLBDSOOFPP01 D"
+  },
+  {
+    "course_name": "Mathematik Lineare Algebra",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 2,
+    "status": "CMP",
+    "score": 90,
+    "short_code": "DLBBIMOI"
+  },
+  {
+    "course_name": "Interkulturelle und ethische Handlungskompetenz",
+    "ects": 5,
+    "exam_type": "CS",
+    "semester": 2,
+    "status": "ENR",
+    "short_code": "DLBIHKOI"
+  },
+  {
+    "course_name": "Statistik Induktive Statistik",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 2,
+    "status": "AVL",
+    "short_code": "DLBOSSIS01 D"
+  },
+  {
+    "course_name": "Cloud Computing",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 3,
+    "status": "AVL",
+    "short_code": "DLBOSCC01 0"
+  },
+  {
+    "course_name": "Projekt: Cloud Programming",
+    "ects": 5,
+    "exam_type": "PO",
+    "semester": 3,
+    "status": "AVL",
+    "short_code": "DLBSEPCP01 0"
+  },
+  {
+    "course_name": "Maschinelles Lernen-Supervised Learning",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 3,
+    "status": "AVL",
+    "short_code": "DLBOSMLSL01 D"
+  },
+  {
+    "course_name": "Maschinelles Lernen-Unsupervised Learning und Feature Engineering",
+    "ects": 5,
+    "exam_type": "CS",
+    "semester": 3,
+    "status": "AVL",
+    "short_code": "DLBDSMLUSL01 D"
+  },
+  {
+    "course_name": "Neuronale Netze und Deep Learning",
+    "ects": 5,
+    "exam_type": "PR",
+    "semester": 3,
+    "status": "AVL",
+    "short_code": "DLBDSNNDL010"
+  },
+  {
+    "course_name": "Einführung in Computer Vision",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 4,
+    "status": "AVL",
+    "short_code": "DLBAIICV01 D"
+  },
+  {
+    "course_name": "Projekt: Computer Vision",
+    "ects": 5,
+    "exam_type": "RE",
+    "semester": 4,
+    "status": "AVL",
+    "short_code": "DLBAIPCV01 D"
+  },
+  {
+    "course_name": "Einführung in das Reinforcement Learning",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 4,
+    "status": "AVL",
+    "short_code": "DLBAURL01 D"
+  },
+  {
+    "course_name": "Einführung in NLP",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 4,
+    "status": "AVL",
+    "short_code": "DUBAIINLP01 D"
+  },
+  {
+    "course_name": "Projekt: NLP",
+    "ects": 5,
+    "exam_type": "RE",
+    "semester": 4,
+    "status": "AVL",
+    "short_code": "DLBAIPNLPOL D"
+  },
+  {
+    "course_name": "Einführung in Datenschutz und IT-Sicherheit",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 5,
+    "status": "AVL",
+    "short_code": "OLBISICAL"
+  },
+  {
+    "course_name": "Data Science Software Engineering",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 5,
+    "status": "AVL",
+    "short_code": "DLBDSDSSE01 D"
+  },
+  {
+    "course_name": "Projekt: Vom Modell zum Produktivsystem",
+    "ects": 5,
+    "exam_type": "PR",
+    "semester": 5,
+    "status": "AVL",
+    "short_code": "DLBDSMTPOI D"
+  },
+  {
+    "course_name": "Seminar: Ethische Fragen der Data Science",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 5,
+    "status": "AVL",
+    "short_code": "DLBDSSECD501 D"
+  },
+  {
+    "course_name": "User Experience",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 5,
+    "status": "AVL",
+    "short_code": "DLBMIUEX01-01"
+  },
+  {
+    "course_name": "UX-Projekt ODER Projekt: Edge AI",
+    "ects": 5,
+    "exam_type": "RE",
+    "semester": 6,
+    "status": "AVL",
+    "short_code": "DLBMIU EX02 OR"
+  },
+  {
+    "course_name": "Einführung in die Robotik",
+    "ects": 5,
+    "exam_type": "WE",
+    "semester": 6,
+    "status": "AVL",
+    "short_code": "DLBROIROL D"
+  },
+  {
+    "course_name": "Projekt: Agiles Projektmanagement",
+    "ects": 5,
+    "exam_type": "RE",
+    "semester": 6,
+    "status": "AVL",
+    "short_code": "DLBOBAPM01"
+  },
+  {
+    "course_name": "Bachelorarbeit",
+    "ects": 9,
+    "exam_type": "TH",
+    "semester": 8,
+    "status": "AVL",
+    "short_code": "BBAKO1"
+  },
+  {
+    "course_name": "Kolloquium",
+    "ects": 1,
+    "exam_type": "PR",
+    "semester": 8,
+    "status": "AVL",
+    "short_code": "BBAK02"
+  }
+]
---
+++ dashboard/management/commands/prefill_database.py
@@ -0,0 +1,98 @@
+import random
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from dashboard.management.commands._courses import sample_courses
+from dashboard.models import Course, Degree, Semester, Exam, Student, Enrollment
+from dashboard.models.quicklink import Quicklink
+
+
+class Command(BaseCommand):
+    help = 'Generates sample objects and saves them to the database'
+
+    def handle(self, *args, **options):
+        try:
+            with transaction.atomic():
+                degree = self._add_degree()
+                self._add_semesters(degree=degree)
+                student = self._add_student(degree=degree)
+                self._add_quicklinks(student=student)
+                self._add_courses_and_exams(degree=degree, student=student)
+                self.stdout.write(self.style.SUCCESS('Database successfully prefilled'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'An error occurred: {str(e)}'))
+            self.stdout.write(self.style.ERROR('All database changes have been rolled back'))
+            raise
+
+    def _add_degree(self) -> Degree:
+        degree = Degree(degree_type="BSC", description="Bachelor of Science (BSc) for Applied Artificial Intelligence")
+        degree.save()
+        return degree
+
+    def _add_semesters(self, degree: Degree):
+        year = 2024
+        for i in range(1, 6):
+            Semester(degree=degree, name=f"Winter {year}/{year + 1}", year=year, start_date=f"{year}-10-01",
+                     end_date=f"{year + 1}-03-31").save()
+            Semester(degree=degree, name=f"Sommer {year + 1}", year=year + 1, start_date=f"{year + 1}-04-01",
+                     end_date=f"{year + 1}-09-30").save()
+            year += 1
+
+    def _add_student(self, degree: Degree):
+        student = Student(
+            name="Felix Asenbauer", first_name="Felix", last_name="Asenbauer",
+            semester_id=2,
+            email="asenbauerfelix@outlook.com", time_model=48, started_on="2024-10-01", degree=degree
+        )
+        student.save()
+        return student
+
+    def _add_quicklinks(self, student: Student):
+        student.quicklinks.create(
+            text="My Campus",
+            url="https://mycampus.iu.org/home",
+            materialIconRef="e88a" # home
+        )
+        student.quicklinks.create(
+            text="FAQs",
+            url="https://mycampus.iu.org/faq",
+            materialIconRef="eb8b" # question_mark
+        )
+        student.quicklinks.create(
+            text="Kursbuchung",
+            url="https://mycampus.iu.org/study-management/study-plan",
+            materialIconRef="e85d" # Assignment
+        )
+        student.quicklinks.create(
+            text="Klausuranmeldung",
+            url="https://mycampus.iu.org/study-management/examinations",
+            materialIconRef="f88d"  # Edit Square
+        )
+        student.quicklinks.create(
+            text="Profil",
+            url="https://mycampus.iu.org/profile",
+            materialIconRef="e855" # Person
+        )
+        student.quicklinks.create(
+            text="Ablaufplan (bis 31.08.25)",
+            url="https://res.cloudinary.com/iugroup/image/upload/v1746189253/sap_ba_angewandte_kuenstliche_intelligenz_180_FS_BAAKI_de_fs_mhwa9j.pdf",
+            materialIconRef="e878" # Event
+        )
+
+    def _add_courses_and_exams(self, degree: Degree, student: Student):
+        for course_data in sample_courses:
+            semester = Semester.objects.get(identifier=course_data["semester"])
+            course = Course.objects.create(
+                identifier=course_data["short_code"],
+                name=course_data["course_name"],
+                degree=degree,
+                semester=semester,
+                description="",
+                ects=course_data["ects"]
+            )
+            course.save()
+            exam = Exam(degree=degree, semester=semester, course=course, exam_type=course_data["exam_type"])
+            exam.save()
+            status = course_data["status"]
+            Enrollment(status=status, student=student, course=course, exam=exam, score=course_data.get("score", None)).save()
